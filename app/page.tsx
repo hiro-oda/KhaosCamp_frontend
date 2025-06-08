715481b06ec9c296bd49bcb0415921547cfff991
odahiroomi <odahiroomi@gmail.com>
2025-06-09 02:02:28 +0900

    change instruction

--- app/page.tsx
+++ app/page.tsx
@@ -91,7 +91,7 @@ function SimpleVoiceAssistant(props: { onConnectButtonClicked: () => void }) {
               onClick={() => props.onConnectButtonClicked()}
             >
             <span>会話を始める</span>
-            <span className="text-xs tracking-wide">ミュートを解除したらクリック</span>
+            <span className="text-xs tracking-wide">スピーカーをオンにしてクリック</span>
             </motion.button>
           </motion.div>
         ) : (
@@ -141,7 +141,7 @@ function ControlBar(props: { onConnectButtonClicked: () => void }) {
             onClick={() => props.onConnectButtonClicked()}
           >
             <span>会話を始める</span>
-            <span className="text-xs tracking-wide">--ミュート解除後にクリック--</span>
+            <span className="text-xs tracking-wide">スピーカーをオンにしてクリック</span>
           </motion.button>
         )}
       </AnimatePresence>
